99a47f82db5d9ebfd01ff0b2791b53d4494d77b6
kolbert vladimir <kolbert@yandex.ru>
2019-05-30 16:34:03 +0300

    form to separate file

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,7 @@
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
-
-  let title = "";
-  let subtitle = "";
-  let address = "";
-  let email = "";
-  let description = "";
-  let imageUrl = "";
-  let type = "";
+  import EditMeetup from "./Meetups/EditMeetup.svelte";
 
   let meetups = [
     {
@@ -37,6 +30,8 @@
       isFavorite: false
     }
   ];
+
+  let editMode = null;
 
   function addMeetup() {
     const newMeetup = {
@@ -67,58 +62,16 @@
   main {
     margin-top: 5rem;
   }
-
-  form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
-  }
 </style>
 
 <Header />
 
 <main>
 
-  <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-      id="title"
-      label="Title"
-      value={title}
-      on:input={event => (title = event.target.value)} />
-
-    <TextInput
-      id="subtitle"
-      label="Subtitle"
-      value={subtitle}
-      on:input={event => (subtitle = event.target.value)} />
-
-    <TextInput
-      id="address"
-      label="Address"
-      value={address}
-      on:input={event => (address = event.target.value)} />
-
-    <TextInput
-      id="imageUrl"
-      label="Image URL"
-      value={imageUrl}
-      on:input={event => (imageUrl = event.target.value)} />
-
-    <TextInput
-      id="email"
-      label="E-mail"
-      value={email}
-      type="email"
-      on:input={event => (email = event.target.value)} />
-
-    <TextInput
-      id="description"
-      label="Description"
-      value={description}
-      controlType="textarea"
-      on:input={event => (description = event.target.value)} />
-    <Button type="submit" caption="Save" />
-  </form>
+  <Button caption="New meetup" on:click={() => (editMode = 'add')} />
+  {#if editMode}
+    <EditMeetup />
+  {/if}
 
   <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 
