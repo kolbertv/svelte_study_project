772e9e3442ddc1f0991f91d5e7e13fbaf79891f1
vladimir <kolbert@yandex.ru>
2019-06-18 23:34:44 +0300

    add firebase test

---
+++ src/App1.svelte
@@ -0,0 +1,35 @@
+<script>
+  let hobbies = [];
+  let hobbyInput;
+
+  function addHobby() {
+    hobbies = [...hobbies, hobbyInput.value];
+
+    fetch("https://svelte-course-123.firebaseio.com/hobbies.json", {
+      method: "POST",
+      body: JSON.stringify(hobbies),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+            throw new Error('Failed!')
+        }
+        //....
+      })
+      .catch(err => console.log(err));
+  }
+</script>
+
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add hobby</button>
+
+<ul>
+
+  {#each hobbies as hobby}
+    <li>{hobby}</li>
+  {/each}
+
+</ul>
