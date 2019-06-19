56b2970e20f3565b2c74bf94c5f5ac67ee45cdb6
kolbert vladimir <kolbert@yandex.ru>
2019-06-19 15:56:26 +0300

    add store and other for study

--- src/App1.svelte
+++ src/App1.svelte
@@ -1,24 +1,55 @@
 <script>
+  import { onMount } from "svelte";
+  import hobbyStore from "./hobby-store.js";
+
   let hobbies = [];
   let hobbyInput;
+  let isLoading = false;
+
+  isLoading = true;
+  onMount(() => {
+    fetch("https://svelte-course-123.firebaseio.com/hobbies.json")
+      .then(res => {
+        isLoading = false;
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then(data => {
+        // hobbies = Object.values(data);
+        hobbyStore.setHobbies(Object.values(data));
+      })
+      .catch(err => {
+        isLoading = false;
+        console.log(err);
+      });
+  });
 
   function addHobby() {
-    hobbies = [...hobbies, hobbyInput.value];
+    // hobbies = [...hobbies, hobbyInput.value];
+    hobbyStore.addHobbies(hobbyInput.value);
 
+    isLoading = true;
     fetch("https://svelte-course-123.firebaseio.com/hobbies.json", {
       method: "POST",
-      body: JSON.stringify(hobbies),
+      body: JSON.stringify(hobbyInput.value),
       headers: {
         "Content-Type": "application/json"
       }
     })
       .then(res => {
+        isLoading = false;
         if (!res.ok) {
-            throw new Error('Failed!')
+          throw new Error("Failed!");
         }
         //....
+        // alert("Saved data");
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        isLoading = false;
+        console.log(err);
+      });
   }
 </script>
 
@@ -26,10 +57,24 @@
 <input type="text" id="hobby" bind:this={hobbyInput} />
 <button on:click={addHobby}>Add hobby</button>
 
-<ul>
+{#if isLoading}
+  <p>Loading....</p>
+{:else}
+  <ul>
+    {#each $hobbyStore as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{/if}
 
-  {#each hobbies as hobby}
-    <li>{hobby}</li>
-  {/each}
-
-</ul>
+<!-- {#await getHobbies}
+  <p>Loading...</p>
+{:then hobbyData}
+  <ul>
+    {#each hobbyData as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{:catch error}
+  <p>error.message</p>
+{/await} -->
